a0dcafff84e2d3858bb516f29a91f44baea5fcc2
Bruno do Nascimento Silva <bruno.dnascimento@kroton.com.br>
2019-08-28 12:16:50 -0300

    Deploy inicial do projeto ProAgil Eventos

---
+++ ProAgil.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebApi.Data;
+using ProAgil.WebApi.Model;
+
+namespace ProAgil.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext context;
+        public ValuesController(DataContext dataContext)
+        {
+            context = dataContext;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await this.context.Eventos.ToListAsync();                
+
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);   
+            }            
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var result = await this.context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+
+                if (result == null){return NotFound();}    
+
+                return Ok(result);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);   
+            }                        
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
